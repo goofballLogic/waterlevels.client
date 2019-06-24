1bca20595b535054a78adf803cdb409d3be1ff1c
Andrew Gibson <andrew@goofballLogic.com>
2019-06-24 20:22:32 +0100

    packed new version, tests

--- test/test.js
+++ test/test.js
@@ -12,6 +12,10 @@ const client = clientFactory( process );
     const days = await client.listDays( "waterlevel.ie", "2019-01-01", "2019-06-30" );
     assert( days.find( x => x === "2019-06-17" ), "2019-06-17 not found in the list of days for waterlevel.ie" );
 
+    console.log( "Query all days for provider" );
+    const allDays = await client.listDays( "waterlevel.ie" );
+    assert( days.find( x => x === "2019-06-17" ), "2019-06-17 not found in the list of all days for waterlevel.ie" );
+
     console.log( "Query for day's data" );
     const data = await client.getData( "waterlevel.ie", "2019-06-17" );
     assert( data.filter( x => x.name === "Ladyswell" ).length > 1, "Results for Ladyswell not found in the list of measurements for waterlevel.ie on 2019-06-17" );
